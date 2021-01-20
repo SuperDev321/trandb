7b688b66fa37360f39f5b112718479fa107e7146
NikitaNevolin321 <nik.nevolin.02@mail.ru>
2021-01-20 18:22:45 +0300

    fix: add private resize

--- client/src/components/PrivateChat/PrivateChatContent/index.js
+++ client/src/components/PrivateChat/PrivateChatContent/index.js
@@ -1,17 +1,22 @@
-import React, { memo, useState, useEffect, useImperativeHandle, forwardRef } from 'react';
+import React, { memo, useState, useRef, useEffect, useImperativeHandle, forwardRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     Avatar,
     Paper,
     Box,
-    IconButton
+    IconButton,
+    SvgIcon
 } from '@material-ui/core';
-import CloseIcon from '@material-ui/icons/Close';
+import {
+    Close,
+    Minimize
+} from '@material-ui/icons';
 import ChatForm from '../../ChatForm';
 import Draggable from 'react-draggable';
 import PrivateMessageList from '../../PrivateMessageList';
 import {getPrivateMessages} from '../../../utils';
-import { ref } from 'yup';
+import {ReactComponent as Maximium} from './square.svg'
+import {ReactComponent as Restore} from './restore.svg'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,9 +25,9 @@ const useStyles = makeStyles((theme) => ({
         bottom: 0,
         zIndex: 200,
         width: 400,
-        height: 350,
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        overflow: 'hidden',
     },
     header: {
         height: 45,
@@ -35,7 +40,8 @@ const useStyles = makeStyles((theme) => ({
         color: theme.palette.primary.contrastText
     },
     headerContent: {
-        cursor: 'move',
+        cursor: props =>
+            !props.max && 'move',
         userSelect: 'none',
         flexGrow: 1,
         height: '100%',
@@ -51,8 +57,14 @@ const useStyles = makeStyles((theme) => ({
         height: theme.spacing(4),
         marginLeft: 10,
     },
+    main: {
+        flexGrow: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        height: 300,
+    },
     content: {
-        height: 250,
         width: '100%',
         overflow: 'auto',
         flexGrow: 1,
@@ -62,13 +74,52 @@ const useStyles = makeStyles((theme) => ({
     },
     active: {
         zIndex: 201
+    },
+    min: {
+        maxHeight: 45,
+        width: '200px !important',
+        transform: 'none !important',
+    },
+    max: {
+        transform: 'none !important',
+        top: 0,
+        left: 0,
+        minWidth: '100%',
+    },
+    icon: {
+        lineHeight: 0,
+        padding: '4px',
+        cursor: 'pointer'
     }
+    // svg: {
+    //     fill: theme.palette.getContrastText(theme.palette.primary),
+    //     padding: 5
+    // }
 }));
 
 const PrivateChat = ({ me, to, sendMessage, active, setActive }, ref) => {
-    const classes = useStyles();
+    const winRef = useRef();
     const [messages, setMessages] = useState([]);
     const [hide, setHide] = useState(false);
+    const [min, setMin] = useState(false);
+    const [max, setMax] = useState(false);
+    const classes = useStyles({max});
+
+    const handleMinimize = () => {
+        if(min) {
+            setMin(false);
+        } else {
+            setMax(false);
+            setMin(true);
+        }
+    }
+    const handleMaximize = () => {
+        if(max) {
+            setMax(false);
+        } else {
+            setMax(true);
+        }
+    }
 
     useImperativeHandle(ref, () => ({
         show: () => {
@@ -78,9 +129,10 @@ const PrivateChat = ({ me, to, sendMessage, active, setActive }, ref) => {
             return !hide;
         },
         addMessage: (message) => {
-            setMessages([...messages, message])
+            setMessages([...messages, message]);
         }
     }));
+
     useEffect(() => {
         getPrivateMessages({from: me.username, to: to.username},
             (data) => {
@@ -92,24 +144,41 @@ const PrivateChat = ({ me, to, sendMessage, active, setActive }, ref) => {
         );
     }, [me, to])
     return (
-        <Draggable bounds="parent" handle='#private-header' scale={1} onMouseDown={() => {setActive(to.username)}}
+        <Draggable bounds="parent" disabled={max}
+            handle='#private-header' scale={1} onMouseDown={() => {setActive(to.username)}}
         >
-            <Paper className={`${classes.root} ${hide&&classes.hide} ${active&&classes.active}`} >
+            <Paper ref={winRef}
+                className={`${classes.root} ${hide&&classes.hide} ${active&&classes.active} ${max && classes.max} ${min && classes.min}`}
+            >
                 <div className={classes.header} >
                     <Avatar className={classes.smallAvatar}
                         src='/img/default_avatar.png'
                     />
                     <div id="private-header" className={classes.headerContent}>{to.username}</div>
-                    <IconButton color='inherit' size='small'
+                    <div className={classes.icon}
+                        onClick={handleMinimize}
+                        style={{marginBottom: 3}}
+                    >
+                        <Minimize  />
+                    </div>
+                    <div className={classes.icon}
+                        onClick={handleMaximize}
+                    >
+                        <SvgIcon style={{fontSize: '1.3em'}} component={max? Restore: Maximium} viewBox="0 0 600 476.6" />
+                    </div>
+                    <div className={classes.icon}
                         onClick={()=>{setHide(true);}}
                     >
-                        <CloseIcon />
-                    </IconButton>
+                        <Close />
+                    </div>
                 </div>
-                <div className={classes.content}>
-                    <PrivateMessageList messages={messages} me={me}/>
+                <div className={classes.main}>
+                    <div className={classes.content}>
+                        <PrivateMessageList messages={messages} me={me}/>
+                    </div>
+                    <ChatForm to={to.username} sendMessage={sendMessage}/>
                 </div>
-                <ChatForm to={to.username} sendMessage={sendMessage}/>
+                
             </Paper>
         </Draggable>
     );
