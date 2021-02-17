70e7cb829445e799f40c87967f10b40e77dbfcb9
NikitaNevolin321 <nik.nevolin.02@mail.ru>
2021-02-17 16:14:33 +0900

    add admin Forbidden word page

--- server/src/controllers/rooms/getRoomDetail.js
+++ server/src/controllers/rooms/getRoomDetail.js
@@ -1,17 +1,22 @@
 const { Rooms, Bans, Users } = require('../../database/models');
-
+const ipInt = require('ip-to-int');
+const ipToInt = require('ip-to-int');
 const getRoomDetail = async (req, res, next) => {
     try {
         let {roomName} = req.params;
         let userId = req.userData._id;
+        let role =  req.userData.role;
         
         if(roomName) {
             let {_id, name, owner, moderators, category, description, welcomeMessage, maxUsers, cover, icon} = await Rooms.findOne({name: roomName});
             console.log(owner, userId)
-            if(owner.equals(userId)) {
-                let bans = await Bans.find({room: roomName});
+            if(owner.equals(userId) || role === 'admin' || role === 'super_admin') {
+                let bans = await Bans.find({room: roomName, type: 'ip'});
                 owner = await Users.findOne({_id: owner});
-                bans = bans.map(({_id, username}) => ({_id, username}));
+                bans = bans.map(({_id, username, ip}) => {
+                    let realIp = ipToInt(ip).toIP();
+                    return {_id, username, ip: realIp};
+                });
                 moderators = await Users.find({ _id: { $in: moderators? moderators: [] } });
                 moderators = moderators.map(({_id, username, role, gender}) => ({_id, username, role, gender}));
                 return res.status(200).json({
@@ -31,6 +36,7 @@ const getRoomDetail = async (req, res, next) => {
             next(new Error('Can not get empty room'));
         }
     } catch (err) {
+        console.log(err);
         return next(err);
     }
 };
