ac2c2de286a7ce3112528e43a8c7d705d087cf58
NikitaNevolin321 <nik.nevolin.02@mail.ru>
2021-03-10 21:02:03 +0900

    fix checkUser

--- server/src/utils/user/checkUserFromServer.js
+++ server/src/utils/user/checkUserFromServer.js
@@ -3,26 +3,31 @@ const bcrypt = require('bcrypt');
 const checkUserFromServer = async (username, password) => {
     return new Promise((resolve, reject) => {
         let sql = "SELECT a.id, a.username, a.password, IF(ISNULL(b.value), 'male', b.value) as gender, c.group_id as top_group, IF(ISNULL(d.thumb), '', d.thumb) as thumb ";
-          sql += 'FROM ixt0j_users a ';
-          sql += 'LEFT JOIN ixt0j_community_fields_values b ON a.id = b.user_id AND b.field_id = 2 ';
-          sql += 'LEFT JOIN ixt0j_user_usergroup_map c ON a.id = c.user_id ';
-          sql += 'LEFT JOIN ixt0j_community_users d ON a.id = d.userid ';
-          sql += 'WHERE a.username = ? ORDER BY c.group_id DESC LIMIT 1';
-        let last_user = await db.query(sql, username);
+        sql += 'FROM ixt0j_users a ';
+        sql += 'LEFT JOIN ixt0j_community_fields_values b ON a.id = b.user_id AND b.field_id = 2 ';
+        sql += 'LEFT JOIN ixt0j_user_usergroup_map c ON a.id = c.user_id ';
+        sql += 'LEFT JOIN ixt0j_community_users d ON a.id = d.userid ';
+        sql += 'WHERE a.username = ? ORDER BY c.group_id DESC LIMIT 1';
+        db.query(sql, username, function(err, last_user) {
+            if(err) {
+                return reject('db_error');
+            }
+            let user = last_user[0];
+            if (last_user.length !== 0) {
+                const pwd = user.password.replace("$2y$", "$2a$");
+                if (!bcrypt.compareSync(password, pwd) && !bcrypt.compareSync(password, user.password)) {
+                    return reject('password_error');
+                }
+                if (user.block) {
+                    return reject('confirm_error');
+                }
+                return resolve(user);
+            } else {
+                return reject('username_error');
+            }
+        });
            
-        let user = last_user[0];
-        if (last_user.length !== 0) {
-            const pwd = user.password.replace("$2y$", "$2a$");
-            if (!bcrypt.compareSync(password, pwd) && !bcrypt.compareSync(password, user.password)) {
-                return reject('password_error');
-            }
-            if (user.block) {
-                return reject('confirm_error');
-            }
-            return resolve(user);
-        } else {
-            return reject('username_error');
-        }
+        
     });
 }
 
