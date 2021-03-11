3c8e7d0969604df8f7cbd027990cb70120717b5e
NikitaNevolin321 <nik.nevolin.02@mail.ru>
2021-03-12 00:26:22 +0900

    fix error messages

--- server/src/utils/user/checkUserFromServer.js
+++ server/src/utils/user/checkUserFromServer.js
@@ -1,7 +1,7 @@
 const db = require('../../database/serverDbConnection');
 const checkUserFromServer = async (username) => {
     return new Promise((resolve, reject) => {
-        let sql = "SELECT id, username from FROM ixt0j_users WHERE username = ?";
+        let sql = "SELECT username FROM ixt0j_users WHERE username = ? LIMIT 1";
         db.query(sql, username, function(err, last_user) {
             if(err) {
                 return reject('db_error');
