ab97da67aeed7ef7fb2da044e89c134ca7d6708b
NikitaNevolin321 <77449282+NikitaNevolin321@users.noreply.github.com>
2021-06-08 07:51:32 -0700

    Create index.js

---
+++ server/src/config/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const config = {
+  db_host: process.env.PRO_DB,
+  db_name: process.env.DB_NAME,
+  ssl_key: process.env.SSL_KEY,
+  ssl_cert: process.env.SSL_CERT,
+  serverPort: process.env.SERVER_PORT,
+  debugging_mode: false,
+  server_image_path: 'img',
+  VERSION: '1.0.50',
+};
+
+module.exports = config;
