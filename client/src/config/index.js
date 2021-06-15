192fbf107e67a52bc2c775a825a3f0be0c45388d
NikitaNevolin321 <77449282+NikitaNevolin321@users.noreply.github.com>
2021-06-15 08:52:21 -0700

    Update index.js

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,14 +1,8 @@
-require('dotenv').config();
-
 const config = {
-  db_host: process.env.PRO_DB,
-  db_name: process.env.DB_NAME,
-  ssl_key: process.env.SSL_KEY,
-  ssl_cert: process.env.SSL_CERT,
-  serverPort: process.env.SERVER_PORT,
-  debugging_mode: false,
-  server_image_path: 'img',
-  VERSION: '1.0.50',
+  media_server_url: 'https://www.trandlike.com:2087/',
+  server_url: 'https://chat.trandb.com:443',
+  image_path: 'public/img',
+  main_site_url: 'https://www.trand.co.il/',
 };
 
 module.exports = config;
