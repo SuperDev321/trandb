69719f783f74ad82fc7cb491d299a574bd175496
NikitaNevolin321 <nik.nevolin.02@mail.ru>
2021-06-23 18:12:53 -0700

    fix path

--- server/src/controllers/gifts/addGift.js
+++ server/src/controllers/gifts/addGift.js
@@ -20,6 +20,7 @@ const addGift = async (req, res, next) => {
             var giftFile = req.files.gift_file;
             var newGiftFileName = getFileName("icon_", giftFile.name);
             try {
+                await giftFile.mv(path.join(__dirname, '..', '..', '..', '..', 'client', 'build/gifts/', newGiftFileName));
                 await giftFile.mv(path.join(__dirname, '..', '..', '..', '..', 'client', 'public/gifts/', newGiftFileName));
                 await Gifts.create({ name, detail, cost, src: newGiftFileName });
                 res
